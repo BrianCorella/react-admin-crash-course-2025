4e4bdfc0993b021e11a212e327b4b579796cdb98
BrianCorella <briancorella@gmail.com>
2025-04-05 17:43:05 -0600

    Video16:59-Custom text, <FunctionField>

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { Datagrid, List, ReferenceField, TextField } from "react-admin";
+import { Datagrid, List, ReferenceField, TextField, FunctionField } from "react-admin";
 
 const PostList = () => (
     <List>
         <Datagrid>
             <TextField source="id" />
-            <TextField source="title" />
-            <TextField source="body" />
+            <TextField source="title" label="Post Title" />
+            <FunctionField label="Excerpt" render={(record) => `${record.body.substring(0, 50)}...`} />
             <ReferenceField source="userId" reference="users" />
         </Datagrid>
     </List>
