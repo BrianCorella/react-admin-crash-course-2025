e234d5e3be5ad7c213b2ff39f750a935381ff1f5
BrianCorella <briancorella@gmail.com>
2025-04-05 17:25:08 -0600

    Video13:32-<List><Datagrid> components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { Admin } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
+import PostList from "./pages/posts/post-list";
 
 export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider}></Admin>
+  <Admin layout={Layout} dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} />
+  </Admin>
 );
---
+++ src/pages/posts/post-list.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { Datagrid, List, ReferenceField, TextField } from "react-admin";
+
+const PostList = () => (
+    <List>
+        <Datagrid>
+            <TextField source="id" />
+            <TextField source="title" />
+            <TextField source="body" />
+            <ReferenceField source="userId" reference="users" />
+        </Datagrid>
+    </List>
+);
+
+export default PostList;
